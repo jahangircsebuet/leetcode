41209d69b97c873b37e38fd5d300df8935cfd7bc
khalidNascenia <khalid@nascenia.com>
2016-12-30 17:03:21 +0600

    initial commit

---
+++ 344/448.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    int arr[] = {4,1,3,2};
+    vector<int> v(arr, arr+4);
+    for(i=0;i<v.size();i++) {
+        v1[v[i]%s] = 0;
+    }
+    return 0;
+}
